ef20641b957344c46b552e3d003be96560f9db9b
Adrian Guzman <Guzartz@Gs-MacBook-Air.local>
2018-02-05 00:46:14 -0500

    Integrated skills via model generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180205031434) do
+# ActiveRecord::Schema.define(version: 20180205031434) do
+
+ActiveRecord::Schema.define(version: 20180205044609) do
+>>>>>>> model-generator
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +26,13 @@ ActiveRecord::Schema.define(version: 20180205031434) do
   end
 
   create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_utilized"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skils", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
     t.datetime "created_at", null: false
