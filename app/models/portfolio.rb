430244a0122aa13fedfd2d82d311d2caf9261ad6
Adrian Guzman <Guzartz@Gs-MacBook-Air.local>
2018-02-14 02:31:04 -0500

    Implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,4 +7,11 @@ class Portfolio < ApplicationRecord
 		where(subtitle: 'Java')
 	end
 
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "http://placehold.it/600x400"
+		self.thumb_image ||=  "http://placehold.it/350x200"
+	end
+
 end
