2c9692784f5db116abb1b3e8cc47fee124f4bf8f
Adrian Guzman <Guzartz@Gs-MacBook-Air.local>
2018-02-27 18:29:01 -0500

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    CWRUViewTool::Renderer.copyright 'Adrian Guzman', 'All rights reserved'
+  end
 end
