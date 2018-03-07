b46011ab2536bfb5e832039e918affc512e48196
Adrian Guzman <Guzartz@Gs-MacBook-Air.local>
2018-03-06 22:44:54 -0500

    Installed jquery ui, sortable html5 library, and css calls

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,7 @@
 //= require bootstrap/dist/js/bootstrap
 //= require bootstrap-sprockets
 //= require rails-ujs
+//= require jquery-ui
+//= require html.sortable
 //= require turbolinks
 //= require_tree .
