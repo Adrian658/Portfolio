1ebb0340a4e4b6e945377709831408e381e267a0
Adrian Guzman <Guzartz@Gs-MacBook-Air.local>
2018-03-06 22:29:44 -0500

    Updated portfolio to include attr for position

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,7 +4,7 @@ class PortfoliosController < ApplicationController
 	layout "portfolios"
 
 	def index
-		@portfolio_items = Portfolio.all
+		@portfolio_items = Portfolio.by_position
 	end
 
 	def new
