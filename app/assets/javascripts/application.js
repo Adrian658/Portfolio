e0a9e48100a38e4e06fab98c46dd8e634583ca64
Adrian Guzman <afg30@case.edu>
2019-08-17 18:22:17 -0500

    Added requires to js for cocoon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery-ui
+//= require cocoon
+//= require gritter
 //= require html.sortable
 //= require cable
 //= require_tree .
